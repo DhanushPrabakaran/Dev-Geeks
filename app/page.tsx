1c0277f770e7d89d022e0c3b3cf8b262249f459f
Dhanush Prabakaran <105353897+DhanushPrabakaran@users.noreply.github.com>
2023-09-13 16:42:55 +0530

    changes in layout

--- app/page.tsx
+++ app/page.tsx
@@ -9,41 +9,23 @@ export default function Home() {
     <main className="flex flex-col">
       <Header />
       <Drawer />
-      <div className="grid-cols-12 gap-1 grid">
-        <div className="lg:col-span-2 col-span-3 flex flex-col h-screen overflow-hidden bg-base-200">
+      <div className="grid-cols-12 grid">
+        <div className="lg:col-span-2 col-span-3 flex flex-col h-screen overflow-hidden bg-base-200 mr-2">
           <div className="btn m-1">hello</div>
           <div className="btn m-1">hello</div>
           <div className="btn m-1">hello</div>
         </div>
-        <div className="lg:col-span-8 col-span-6   h-screen overflow-y-scroll  ">
+        <div className="lg:col-span-8 col-span-6   h-screen overflow-y-scroll overflow-x-hidden scroll-smooth flex flex-col ">
           <div
           className="text-4xl text-center m-4">
             Projects
           </div>
-          <div
-          className="bg-base-200 hover:border hover:bg-base-100 hover:border-stone-500 w-full h-fit mt-2 p-2">
-            <h1
-            className=" text-2xl">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolorem unde fuga consectetur quis et.
-            </h1>
-            <p
-            className=" text-neutral-700">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus in deleniti magnam exercitationem nobis! Minima laboriosam labore maxime nostrum suscipit explicabo obcaecati, temporibus fuga fugiat dignissimos voluptates quod quaerat magni!
-            </p>
-            <div
-            className=" w-full flex justify-end">
-              
-              <div
-              className="btn bg-black m-1  py-0 mr-3 mb-2 text-xl">
-              view
-              </div>
-            </div>
-          </div>
+          
           <Container/>
           <Container/>
           <Container/>
         </div>
-        <div className="lg:col-span-2  col-span-3 flex flex-col h-screen overflow-hidden bg-base-200">
+        <div className="lg:col-span-2  col-span-3 flex flex-col h-screen overflow-hidden bg-base-200 ">
           <div className="btn m-1">hello</div>
           <div className="btn m-1">hello</div>
           <div className="btn m-1">hello</div>
--- components/Container.tsx
+++ components/Container.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import Image from 'next/image'
+import Profile from '@/public/profile.svg'
 const Container = () => {
   return (
-    <div className="">
+    
     
     <div
-    className="bg-base-200 hover:border hover:bg-base-100 hover:border-stone-500 w-full h-fit mt-2 p-2">
+    // className="bg-base-200 hover:border hover:bg-base-100 hover:border-stone-500 w-full h-fit mt-2 p-2">
+    className="bg-base-200 border-t-4 border-stone-400  rounded-md  h-fit mr-2 my-1 p-2">
       <h1
       className=" text-2xl">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolorem unde fuga consectetur quis et.
@@ -15,15 +17,20 @@ const Container = () => {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus in deleniti magnam exercitationem nobis! Minima laboriosam labore maxime nostrum suscipit explicabo obcaecati, temporibus fuga fugiat dignissimos voluptates quod quaerat magni!
       </p>
       <div
-      className=" w-full flex justify-end">
-        
+      className=" w-full flex justify-between items-center">
         <div
-        className="btn bg-black m-1  py-0 mr-3 mb-2 text-xl">
+        className='flex items-center'>
+        <Image src={Profile} width={35} height={35} alt="Picture of the author" />
+        <p>Created By user</p>
+        </div>
+         
+        <div
+        className="btn bg-black text-xl">
         view
         </div>
       </div>
     </div>
-  </div>
+  
   )
 }
 
