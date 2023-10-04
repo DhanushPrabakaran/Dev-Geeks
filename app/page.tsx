0566bb87765b92ed8638892587f4603f8ddad64e
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2023-10-04 15:39:42 +0530

    typ

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Header from "@/components/Header";
 import Drawer from "@/components/Drawer";
 import CopyRight from "@/components/CopyRight";
@@ -6,7 +6,7 @@ import Footer from "@/components/Footer";
 import Container from "@/components/Container";
 import Navbar from "@/components/Navbar";
 import Rightbar from "@/components/Rightbar";
-import Typewriter from 'react-ts-typewriter';
+import Typewriter from "react-ts-typewriter";
 export default function Home() {
   return (
     <main className="flex flex-col">
@@ -17,19 +17,28 @@ export default function Home() {
           <Navbar />
         </div>
         <div className=" flex-1 h-screen grow-1  overflow-y-scroll overflow-x-hidden scroll-smooth flex flex-col  ">
-          
           <div className="hero lg:h-full  h-full bg-base-200">
             <div className="hero-content flex-col">
               <div className=" flex items-center flex-col ">
                 <h1 className=" text-6xl font-sans font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-secondary via-success to-secondary mb-2 ">
-
                   WELCOME YOU ALL
                 </h1>
-                <div
-                className=" text-2xl text-center w-full mb-2 ">
-               we help you to
-               <br />  <span
-               className=" text-primary text-4xl text-justify"><Typewriter text={ ["ideate" , "discuss", "collabrate" , "dev\nelop","test", "relese"]}  speed={500} /></span>
+                <div className=" text-2xl text-center w-full mb-2 ">
+                  we help you to
+                  <br />{" "}
+                  <span className=" text-primary text-4xl text-justify">
+                    <Typewriter
+                      text={[
+                        "ideate",
+                        "discuss",
+                        "collabrate",
+                        "dev\nelop",
+                        "test",
+                        "relese",
+                      ]}
+                      speed={500}
+                    />
+                  </span>
                 </div>
                 <button className="btn btn-primary">Get Started</button>
               </div>
@@ -42,10 +51,6 @@ export default function Home() {
                       and gater reviesws
                     </div>
                   </div>
-
-                  
-
-                  
                 </div>
               </div>
             </div>
