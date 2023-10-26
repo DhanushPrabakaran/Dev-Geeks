e5ad6ced602c65b762b46b21a87591d27f958fe9
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2023-10-26 21:46:05 +0530

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useRef } from "react";
 import logo from "@/public/next.svg";
 import Image from "next/image";
-import Profile from "@/public/profile.svg";
+import Profile from "@/public/profilepic.jpeg";
 const Header = () => {
   const inputRef = useRef(null);
   const keyDownHandler = (event: KeyboardEvent) => {
@@ -18,7 +18,7 @@ const Header = () => {
   return (
     <div className="navbar   h-5 w-auto  border-b border-b-stone-500 bg-base-100">
       <div className="flex-none lg:hidden min-md:visible">
-        {/* <button className="btn btn-square btn-ghost"> */}
+        <button className="btn btn-square btn-ghost">
         <label htmlFor="my-drawer" className="btn btn-square btn-ghost">
           <svg
             fill="none"
@@ -34,28 +34,27 @@ const Header = () => {
           </svg>
         </label>
 
-        {/* </button> */}
+        </button>
       </div>
-      <div className=" gap-2 flex-1 justify-between ">
-        <div className="flex items-center ">
-          <a className="btn btn-ghost text-lg normal-case mr-2"><span className=" text-secondary font-extrabold">Dev</span>Geeks</a>
-          <div className="form-control relative  ">
+      <div className=" flex-1 justify-between ">
+        <div className="flex items-center justify-between flex-grow mx-1 ">
+          <a className=" mr-1  normal-case font-bold from-primary via-primary-focus to-slate-300 ">DevGeeks</a>
+          {/* <div className="form-control relative  ">
             <input
               ref={inputRef}
               type="text"
               placeholder="Search here..."
               className="input  input-bordered input-sm input-ghost"
             />
-
             <span className=" absolute right-1 top-1  ">
               <kbd className="kbd kbd-sm ">alt</kbd> +
               <kbd className="kbd kbd-sm">l</kbd>
             </span>
-          </div>
+          </div> */}
         </div>
         <div className="dropdown dropdown-end">
           <label tabIndex={0} className="btn btn-neutral btn-circle avatar">
-            <div className="w-10 rounded-full">
+            <div className="w-7 rounded-full">
               <Image src={Profile} alt="Picture of the author" />
             </div>
           </label>
@@ -64,10 +63,10 @@ const Header = () => {
             className="mt-3 z-[1] p-2 shadow menu menu-md dropdown-content bg-base-100 rounded-box w-40"
           >
             <li>
-              <a className="justify-center btn-neutral">Profile</a>
+              <a className="justify-center btn-neutral m-1">Profile</a>
             </li>
             <li>
-              <a className="justify-center btn-error">Logout</a>
+              <a className="justify-center btn-error m-1">Logout</a>
             </li>
           </ul>
         </div>
