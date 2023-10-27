ccf7a3acb001b8d63ace9a062da9bd8eed18d63c
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2023-10-27 22:35:41 +0530

    upload

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,20 +1,21 @@
-import NextAuth, { AuthOptions } from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
+import NextAuth from "next-auth";
+import {OPTIONS} from "@/app/api/auth/[...nextauth]/options"
 
-export const OPTIONS: AuthOptions = {
-    providers: [
-      GitHubProvider({
-        name: "GitHub",
-        clientId: process.env.GITHUB_ID!,
-        clientSecret: process.env.GITHUB_SECRET!
-      })
-    ],
-    // pages: {
-    //     signIn: "/signin",
-    //     signOut: "/signout"
-    //   }
+
+// export const OPTIONS: AuthOptions = {
+//     providers: [
+//       GitHubProvider({
+//         name: "GitHub",
+//         clientId: process.env.GITHUB_ID!,
+//         clientSecret: process.env.GITHUB_SECRET!
+//       })
+//     ],
+//     // pages: {
+//     //     signIn: "/signin",
+//     //     signOut: "/signout"
+//     //   }
     
-  };
+//   };
 
 
 const handler = NextAuth(OPTIONS);
